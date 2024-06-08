3198279f96086e249726cc63c203369a878568e8
Daniel Mombeyni <mombeyni.daniel@gmail.com>
2024-06-08 15:44:35 +0330

    add model recipe & test recipe

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_spectacular",
+    "rest_framework.authtoken",
     "core",
+    "user",
+    "recipe",
 ]
 
 MIDDLEWARE = [
@@ -95,3 +100,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
+}
--- app/core/admin.py
+++ app/core/admin.py
@@ -40,3 +40,5 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+
+admin.site.register(models.Recipe)
